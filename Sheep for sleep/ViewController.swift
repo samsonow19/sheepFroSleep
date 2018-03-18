13943cd09910ac2df964c1d009addae1fee1db26
esamsonov <esamsonov@smedialink.com>
2018-03-18 15:55:47 +0300

    Initial Commit

---
+++ Sheep for sleep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sheep for sleep
+//
+//  Created by Evgeniy Samsonov on 18.03.18.
+//  Copyright © 2018 Evgeniy Samsonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
